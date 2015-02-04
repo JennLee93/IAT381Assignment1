3a057fd39717db5b79447d02b6d4d7ac81b9d99f
JennLee93 <jennifer93lee@gmail.com>
2015-02-04 15:03:34 -0800

    Create end.js

---
+++ scripts/end.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myappApp.controller:EndCtrl
+ * @description
+ * # AccessoriesCtrl
+ * Controller of the myappApp
+ */
+angular.module('myappApp')
+  .controller('EndCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
